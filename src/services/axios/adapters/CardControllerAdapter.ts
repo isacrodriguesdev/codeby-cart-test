cbcdb8cedc11fced6b9ff513d2ea0561c2a7fb9a
Isac Rodrigues <isacrodriguesdev@protonmail.com>
2022-05-14 09:42:12 -0300

    git init

--- src/services/axios/adapters/CardControllerAdapter.ts
+++ src/services/axios/adapters/CardControllerAdapter.ts
@@ -1,9 +1,12 @@
-import { Product } from '@/models/Product';
-import { ICardControllerAdapter } from '@/services/ICardControllerAdapter';
-import axios from 'axios';
+import axios from "axios";
+import { Product } from "@/models/Product";
+import { ICardControllerAdapter } from "@/services/ICardControllerAdapter";
 
 export class HttpControllerAdapter implements ICardControllerAdapter {
+
   getProducts(): Product[] {
-    return [new Product()];
+    const products = axios.get("")
+    return []
   }
+
 }
